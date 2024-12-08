219f05c388d9a2fa32670b0fb7d289e1b71be801
JaneWang037 <jane.wang@gmx.at>
2024-12-08 15:27:11 +0100

    modify app.PY

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,13 +6,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     sensor_data = {
-        'temperature': 22.5,  
-        'humidity': 60        
+        'temperature': 22.5, 
+        'humidity': 60     
     }
 
     temperature = sensor_data.get('temperature')
     humidity = sensor_data.get('humidity')
-    return render_template('index.html', temperature=temperature, humidity=humidity)
+    return render_template(
+        'index.html', temperature=temperature, humidity=humidity
+        )
 
 
 if __name__ == "__main__":
