9e057eab783c12c98c5f0a394982754a08be172b
JaneWang037 <jane.wang@gmx.at>
2024-12-10 20:56:25 +0100

    modified app and mqtt-client

---
+++ flask-jane-feature-DH11/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    sensor_data = {
+        'temperature': 22.5,
+        'humidity': 60
+    }
+
+    temperature = sensor_data.get('temperature')
+    humidity = sensor_data.get('humidity')
+    return render_template(
+    'index.html',
+    temperature=temperature, 
+    humidity=humidity
+)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
