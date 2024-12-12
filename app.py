0aa0876fd959c270865f844c034a4c6373a642ee
JaneWang037 <jane.wang@gmx.at>
2024-12-12 07:22:00 +0100

    remove certs

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
+from flask_mqtt import Mqtt
 
 app = Flask(__name__)
 
+app.config['MQTT_BROKER_URL'] = '192.168.8.106'
+app.config['MQTT_BROKER_PORT'] = 1883
+# app.config['MQTT_USERNAME'] = ''  # Set this item when you need to verify username and password
+# app.config['MQTT_PASSWORD'] = ''  # Set this item when you need to verify username and password
+app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
+app.config['MQTT_TLS_ENABLED'] = False  # If your broker supports TLS, set it True
+topic = '/sensors/#'
 
-@app.route("/")
-def index():
-    sensor_data = {
-        'temperature': 22.5,
-        'humidity': 60
-    }
-
-    temperature = sensor_data.get('temperature')
-    humidity = sensor_data.get('humidity')
-    return render_template(
-    'index.html', temperature=temperature, humidity=humidity
-)
+mqtt_client = Mqtt(app)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@mqtt_client.on_connect()
+def handle_connect(client, userdata, flags, rc):
+   if rc == 0:
+       print('Connected successfully')
+       mqtt_client.subscribe(topic) # subscribe topic
+   else:
+       print('Bad connection. Code:', rc)
+
+
+@mqtt_client.on_message()
+def handle_mqtt_message(client, userdata, message):
+   data = dict(
+       topic=message.topic,
+       payload=message.payload.decode()
+  )
+   print('Received message on topic: {topic} with payload: {payload}'.format(**data))
+
+
+@app.route('/publish', methods=['POST'])
+def publish_message():
+   request_data = request.get_json()
+   publish_result = mqtt_client.publish(request_data['topic'], request_data['msg'])
+   return jsonify({'code': publish_result[0]})
+
+if __name__ == '__main__':
+   app.run(host='127.0.0.1', port=5000)
