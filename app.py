3279b321388a04fb8deb3536e20437c1c8ee218b
JaneWang037 <jane.wang@gmx.at>
2024-12-12 22:48:25 +0100

    modify formart for linting

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 from flask_mqtt import Mqtt
 
 app = Flask(__name__)
 
 app.config['MQTT_BROKER_URL'] = '192.168.8.106'
 app.config['MQTT_BROKER_PORT'] = 1883
-# app.config['MQTT_USERNAME'] = ''  # Set this item when you need to verify username and password
-# app.config['MQTT_PASSWORD'] = ''  # Set this item when you need to verify username and password
-app.config['MQTT_KEEPALIVE'] = 5  # Set KeepAlive time in seconds
-app.config['MQTT_TLS_ENABLED'] = False  # If your broker supports TLS, set it True
+app.config['MQTT_KEEPALIVE'] = 5 
+app.config['MQTT_TLS_ENABLED'] = False  
 topic = '/sensors/#'
 
 mqtt_client = Mqtt(app)
@@ -17,22 +15,13 @@ messages = []
 
 @mqtt_client.on_connect()
 def handle_connect(client, userdata, flags, rc):
-   if rc == 0:
-       print('Connected successfully')
-       mqtt_client.subscribe(topic) # subscribe topic
-   else:
-       print('Bad connection. Code:', rc)
+    if rc == 0:
+        print('Connected successfully')
+        mqtt_client.subscribe(topic)
+    else:
+        print('Bad connection. Code:', rc)
 
 
-@mqtt_client.on_message()
-def handle_mqtt_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
-    messages.append(data)  
-    print('Received message on topic: {topic} with payload: {payload}'.format(**data))
-    
 @app.route('/')
 def index():
     return '''
@@ -52,7 +41,10 @@ def handle_mqtt_message(client, userdata, message):
 @app.route('/publish', methods=['GET'])
 def view_messages():
     messages_html = ''.join(
-        f'<tr><td>{msg["topic"]}</td><td>{msg["payload"]}</td></tr>' for msg in messages
+        f'<tr>'
+        f'<td>{msg["topic"]}</td>'
+        f'<td>{msg["payload"]}</td>'
+        f'</tr>' for msg in messages
     )
     html = f'''
         <h1>Subscribed Data</h1>
@@ -61,7 +53,11 @@ def view_messages():
                 <th>Topic</th>
                 <th>Payload</th>
             </tr>
-            {messages_html if messages else '<tr><td colspan="2">No messages yet</td></tr>'}
+            {
+                messages_html 
+                if messages 
+                else '<tr><td colspan="2">No messages yet</td></tr>'
+            }
         </table>
         <p><a href="/">Back to Home</a></p>
     '''
